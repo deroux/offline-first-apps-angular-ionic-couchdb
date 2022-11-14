f3d8ff6ed4b7ae5b528227cdb7c7e87c71eede37
deroux <dominicrudigier@gmail.com>
2022-11-14 21:55:07 +0100

    COMMENT: description of unit test

--- todo-test-app/src/app/tab1/tab1.page.spec.ts
+++ todo-test-app/src/app/tab1/tab1.page.spec.ts
@@ -4,22 +4,28 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 
 import { Tab1Page } from './tab1.page';
 
-describe('Tab1Page', () => {
+// jasmin based jargon
+describe('Tab1Page', () => { // creates test suite
   let component: Tab1Page;
   let fixture: ComponentFixture<Tab1Page>;
-
-  beforeEach(waitForAsync(() => {
+  
+  beforeEach(waitForAsync(() => { // setup, runs before each spec (it) test case
+    // Angular based jargon
+    // TestBed: ~ Angular module with dependencies for testing of component
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
-
+    // fixture: wrapper that can access HTML and TS components, use to create components and services for an instance
     fixture = TestBed.createComponent(Tab1Page);
+    // pages in angular are components
     component = fixture.componentInstance;
+    // start angular change detection Lifecycle
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create', () => { // test Case, 1 spec, one test in the suite
     expect(component).toBeTruthy();
   });
 });
+

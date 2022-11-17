1a3709683fc7befc3515567f94d930014ac60124
deroux <dominicrudigier@gmail.com>
2022-11-17 13:19:31 +0100

    feat: first working implementation of 4-way-binding

--- todo-test-app/src/app/tab1/tab1.page.spec.ts
+++ todo-test-app/src/app/tab1/tab1.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { TodoService } from '../services/todo.service';
 
 import { Tab1Page } from './tab1.page';
 
@@ -8,18 +9,29 @@ import { Tab1Page } from './tab1.page';
 describe('Tab1Page', () => { // creates test suite
   let component: Tab1Page;
   let fixture: ComponentFixture<Tab1Page>;
+  let todoService;
+
+  const TodoServiceStub = {
+    addTodo: () => null,
+    getTodos: () => []
+  };
   
   beforeEach(waitForAsync(() => { // setup, runs before each spec (it) test case
     // Angular based jargon
     // TestBed: ~ Angular module with dependencies for testing of component
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule],
+      providers: [{provide: TodoService, useValue: TodoServiceStub}]
     }).compileComponents();
     // fixture: wrapper that can access HTML and TS components, use to create components and services for an instance
     fixture = TestBed.createComponent(Tab1Page);
     // pages in angular are components
     component = fixture.componentInstance;
+
+    // inject service
+    todoService = TestBed.inject(TodoService);
+        
     // start angular change detection Lifecycle
     fixture.detectChanges();
   }));
@@ -39,6 +51,19 @@ describe('Tab1Page', () => { // creates test suite
     
     expect(component.todos.length).toBeGreaterThan(0);
     expect(component.todos).toContain(todo);
+  });
+
+  it('addTodo should have called the todoService addTodo function', () => {
+    const todo = 'My new Todo';
+
+    // inject mock service in a specific test case
+    TestBed.overrideProvider(TodoService, {useValue: TodoServiceStub});
+    spyOn(todoService, 'addTodo');
+
+    component.addTodo(todo);
+
+    expect(todoService.addTodo).toHaveBeenCalled();
+    expect(todoService.addTodo).toHaveBeenCalledWith(todo);
   });
 });
 

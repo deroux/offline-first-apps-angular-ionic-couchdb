76a642971d135e0320985ad2c9e03c0efb014e85
deroux <dominicrudigier@gmail.com>
2022-12-18 09:07:36 +0100

    Manage Tables implemented

--- julies/src/app/model/table.ts
+++ julies/src/app/model/table.ts
@@ -1,11 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class TableDoc {
-  _id: string = '';
+  _id: string = uuidv4();
   _rev: string = '';
   type: string = 'table';
   table: Table = new Table();
 }
 
-class Table {
+export class Table {
   id: string = '';
   guests: number = 0;
   state: string = 'free';
--- julies/src/app/pages/crud-tables/crud-tables.page.ts
+++ julies/src/app/pages/crud-tables/crud-tables.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, PopoverController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { DBRepository } from 'src/app/db/DB.repository';
+import { Table, TableDoc } from 'src/app/model/table';
+import { TableService } from 'src/app/services/table/table.service';
 
 @Component({
   selector: 'app-crud-tables',
@@ -6,10 +11,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud-tables.page.scss'],
 })
 export class CrudTablesPage implements OnInit {
+  tablesDocs: Array<TableDoc> = [];
+  subscriptions: Array<Subscription> = [];
 
-  constructor() { }
+  constructor(
+    private tableService: TableService,
+    private dbService: DBRepository<any>,
+    private popoverCtrl: PopoverController,
+    private alertCtrl: AlertController
+  ) {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.initSubscriptions();
   }
 
+  ionViewWillLeave() {
+    this.unsubscribe();
+  }
+
+  unsubscribe() {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+
+  initSubscriptions() {
+    let p = this.tableService
+      .getCurrentTables()
+      .subscribe((docs: Array<TableDoc>) => {
+        if (docs === undefined || docs.length <= 0) return;
+        this.tablesDocs = docs;
+      });
+    this.subscriptions.push(p);
+  }
+
+  addTable(id: any, guests: any) {
+    if (id === undefined || guests === undefined || id === '' || guests === '')
+      return;
+
+    let tableDoc = new TableDoc();
+    let table: Table = {
+      id: id.toString(),
+      guests: Number(guests),
+      state: 'free',
+    };
+    tableDoc.table = table;
+
+    this.tablesDocs.push(tableDoc);
+    this.dbService.createOrUpdate(tableDoc).then(() => {
+      this.popoverCtrl.dismiss();
+    });
+  }
+
+  editTable(t: TableDoc, id: any, guests: any) {
+    if (id === undefined || guests === undefined || id === '' || guests === '')
+      return;
+
+    t.table.id = id.toString();
+    t.table.guests = Number(guests);
+
+    this.dbService.createOrUpdate(t).then(() => {
+      this.popoverCtrl.dismiss();
+    });
+  }
+
+  async onDelete(t: TableDoc) {
+    const alert = await this.alertCtrl.create({
+      header: `Confirm delete of ${t.table.id}`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {},
+        },
+        {
+          text: 'Confirm',
+          role: 'confirm',
+          handler: () => {
+            this.dbService.delete(t).finally(() => {
+              this.tableService.fetchTables();
+            });
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
